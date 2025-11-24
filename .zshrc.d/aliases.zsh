14f57c7169c594084a2f6abe8095380a32a35902
David <21027243+davidbaptista@users.noreply.github.com>
2025-11-24 23:46:56 +0000

    fix gitignore

---
+++ .zshrc.d/aliases.zsh
@@ -0,0 +1,4 @@
+alias vi="vim"
+alias vim="nvim"
+alias compose="docker compose"
+alias buildx="docker buildx"
---
+++ .zshrc.d/plugins.zsh
@@ -0,0 +1,24 @@
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+autoload -Uz compinit
+compinit
+
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beggining-search
+
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beggining-search
+
+bindkey '^[[A' up-line-or-beginning-search
+bindkey '^[[B' down-line-or-beggining-search
+
+zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*:descriptions' format '%B%d%b' 
+
+setopt CORRECT
+setopt NO_CASE_GLOB
+setopt EXTENDED_GLOB
+setopt LIST_TYPES
+
