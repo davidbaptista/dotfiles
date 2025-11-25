d69b323dd547d67522cc03ab3298aa8c95c326c6
David <21027243+davidbaptista@users.noreply.github.com>
2025-11-25 21:35:36 +0000

    try autosave

--- nvim/lua/config/autosave.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
-  pattern = "*",
-  callback = function()
-    if vim.bo.modifiable and vim.bo.filetype ~= "lazy" then
-      vim.cmd("silent! write")
-    end
-  end,
-})
