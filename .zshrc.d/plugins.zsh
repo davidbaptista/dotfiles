73415dc854ae4fd3099be2d9b6994616f68101c3
David <21027243+davidbaptista@users.noreply.github.com>
2025-11-25 19:28:01 +0000

    fix

--- .zshrc.d/plugins.zsh
+++ .zshrc.d/plugins.zsh
@@ -5,13 +5,13 @@ autoload -Uz compinit
 compinit
 
 autoload -U up-line-or-beginning-search
-autoload -U down-line-or-beggining-search
+autoload -U down-line-or-beginning-search
 
 zle -N up-line-or-beginning-search
-zle -N down-line-or-beggining-search
+zle -N down-line-or-beginning-search
 
 bindkey '^[[A' up-line-or-beginning-search
-bindkey '^[[B' down-line-or-beggining-search
+bindkey '^[[B' down-line-or-beginning-search
 
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
