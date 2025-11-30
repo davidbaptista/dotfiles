bdc75e13a082baf3f0b5fb70a72ce47b2c7b1a92
David <21027243+davidbaptista@users.noreply.github.com>
2025-11-30 17:58:50 +0000

    improve everything

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,18 +5,15 @@ return {
       win = {
         keys = {
           nav_l = false,
+
           clear = {
             "<C-l>",
             function(self)
-              vim.fn.feedkeys("clear\n", "n")
+              vim.api.nvim_chan_send(vim.bo.channel, "\x0c")
             end,
             mode = "t",
             desc = "Clear terminal",
           },
-
-          --          term_left = { "<M-Left>", "<C-\\><C-n>b", mode = "t", desc = "Word left" },
-          --         term_right = { "<M-Right>", "<C-\\><C-n>w", mode = "t", desc = "Word right" },
-          --        term_del = { "<M-BS>", "<C-w>", mode = "t", desc = "Delete word" },
         },
       },
     },
